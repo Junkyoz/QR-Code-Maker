7ebcc5ee7d9e29c35c6b3b74d4c37d3f518d249d
Junkyoz <Lucas.98472@gmail.com>
2024-01-14 22:21:04 +0100

    Update QR-Coder Maker.py

--- QR-Coder Maker.py
+++ QR-Coder Maker.py
@@ -1,4 +1,22 @@
 import qrcode
 import sys
 import os
-import tkinter
+import tkinter as tk
+from PIL import ImageTk, Image
+
+def app():
+    root = tk.Tk()
+    root.title('QR-Code Maker')
+    root.resizable(False, False)
+    root.geometry('400x600')
+    
+    img = ImageTk.PhotoImage()
+    qr_displayed = tk.Label(image=img)
+    qr_displayed.pack()
+    
+    ent = tk.Entry(root)
+    ent.pack()
+        
+    root.mainloop()
+    
+app()
